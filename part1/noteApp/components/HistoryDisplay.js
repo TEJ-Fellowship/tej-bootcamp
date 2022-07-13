2636a9d15985bf706ae035c7d9d9093fa169eebb
rushilshakya <rushil.shakya@gmail.com>
2022-07-13 11:34:11 +0545

    feat: add conditional rendering of history

--- part1/noteApp/components/HistoryDisplay.js
+++ part1/noteApp/components/HistoryDisplay.js
@@ -1,5 +1,9 @@
 const HistoryDisplay = ({ history }) => {
-  return <h1>The history of clicks is: {history}</h1>;
+  if (history.length > 0) {
+    return <h1>The history of clicks is: {history}</h1>;
+  } else {
+    return <h1>There is no history yet</h1>;
+  }
 };
 
 export default HistoryDisplay;
