0db3273edb8f21658b136b903c5aee274c24beca
rushilshakya <rushil.shakya@gmail.com>
2022-07-13 10:45:53 +0545

    feat: use array as state

--- part1/noteApp/components/App.js
+++ part1/noteApp/components/App.js
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 import Display from "./Display";
+import HistoryDisplay from "./HistoryDisplay";
 import MyButton from "./MyButton";
 import Myh1 from "./Myh1";
 
 const App = () => {
   const [myCount, setMyCount] = useState(1);
+  const [clickButtonHistory, setClickButtonHistory] = useState([]);
 
   console.log("calling");
-  const increaseCount = () => setMyCount(myCount + 1);
+  const increaseCountFromApp = () => {
+    setMyCount(myCount + 1);
+    // setClickButtonHistory([...clickButtonHistory, "App"]);
+    setClickButtonHistory(clickButtonHistory.concat("App")); //[], ["App"]
+    // setClickButtonHistory(clickButtonHistory.push("App")); //[], ["App"]
+  };
+
+  const increaseCountFromButton = () => {
+    setMyCount(myCount + 1);
+    setClickButtonHistory([...clickButtonHistory, "Button"]);
+    // setClickButtonHistory([1, 2, 3, 4, "Button"]); //if clickButtonHistory = [1, 2, 3, 4], ["App", "Button"]
+  };
 
   return (
     <div>
       <h3>{myCount}</h3>
       <Display showValue={myCount} />
-      <button onClick={increaseCount}>Click me in same component</button>
-      <MyButton buttonHandler={increaseCount} />
+      <HistoryDisplay history={clickButtonHistory} />
+      <button onClick={increaseCountFromApp}>Click me in same component</button>
+      <MyButton buttonHandler={increaseCountFromButton} />
       <Myh1 name="amir" lastName="some LastName1" count={myCount} />
       <Myh1 name="Niru" lastName="some LastName2" count={myCount} />
       <Myh1 name="Ashim" lastName="some LastName3" count={myCount} />
---
+++ part1/noteApp/components/HistoryDisplay.js
@@ -0,0 +1,5 @@
+const HistoryDisplay = ({ history }) => {
+  return <h1>The history of clicks is: {history}</h1>;
+};
+
+export default HistoryDisplay;
