86647a9255a1dcb43db311fb6e54cfa56aff6050
rushilshakya <rushil.shakya@gmail.com>
2022-07-18 10:24:23 +0545

    feat: complete part2-a

--- part2/notes-app/src/App.js
+++ part2/notes-app/src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Hello world</h1>;
-}
+import Note from "./components/Note";
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
