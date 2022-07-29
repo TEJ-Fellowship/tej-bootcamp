57085064daac3c35f570f698b65651ec035fde61
rushilshakya <rushil.shakya@gmail.com>
2022-07-29 11:33:41 +0545

    feat: fetch data from db in rest api

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const Note = require("./models/note");
+
 const App = express();
 App.use(express.static("build"));
 App.use(cors());
@@ -16,33 +18,15 @@ App.use((request, response, next) => {
 
 // App.use(express.json());
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-1-17T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    date: "2022-1-17T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-1-17T19:20:14.298Z",
-    important: true,
-  },
-];
-
 App.get("/", (request, response) => {
   response.send("<h1>hello world there</h1>");
 });
 
+const notes = [];
+
 App.get("/notes", (request, response) => {
-  response.json(notes);
+  Note.find().then((result) => response.json(result));
+  // response.json(notes);
 });
 
 App.get("/notes/:id", (request, response) => {
