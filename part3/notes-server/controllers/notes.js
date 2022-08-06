0e351c7f442634960404657be7ba0c582f3788f0
rushilshakya <rushil.shakya@gmail.com>
2022-08-06 11:20:25 +0545

    feat: use token in note creation

--- part3/notes-server/controllers/notes.js
+++ part3/notes-server/controllers/notes.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const User = require("../models/user");
@@ -22,23 +23,40 @@ notesRouter.get("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 notesRouter.post("/", async (request, response, next) => {
-  const body = request.body;
+  let savedNote;
+  try {
+    const body = request.body;
 
-  const user = await User.findById(body.userId);
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
 
-  const note = new Note({
-    content: body.content,
-    important: body.important === undefined ? false : body.important,
-    date: new Date(),
-    user: user._id,
-  });
+    const note = new Note({
+      content: body.content,
+      important: body.important === undefined ? false : body.important,
+      date: new Date(),
+      user: user._id,
+    });
 
-  const savedNote = await note.save();
-  user.notes = user.notes.concat(savedNote._id);
-  await user.save();
-
-  response.json(savedNote);
+    savedNote = await note.save();
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
+  } catch (error) {
+    next(error);
+  }
+  response.status(201).json(savedNote);
 });
 
 notesRouter.delete("/:id", (request, response, next) => {
