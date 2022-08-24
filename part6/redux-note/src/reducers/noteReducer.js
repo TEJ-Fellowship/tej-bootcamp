0f2fdfd56b4f328857bf5f4a67dbe0b90e50927b
rushilshakya <rushil.shakya@gmail.com>
2022-08-24 11:35:14 +0545

    feat: call backend and redux

--- part6/redux-note/src/reducers/noteReducer.js
+++ part6/redux-note/src/reducers/noteReducer.js
@@ -1,31 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  {
-    content: "reducer defines how redux store works",
-    important: true,
-    id: 1,
-  },
-  {
-    content: "state of store can contain any data",
-    important: false,
-    id: 2,
-  },
-];
-
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const noteSlice = createSlice({
   name: "notes",
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
-      const content = action.payload;
-      state.push({
-        content,
-        important: false,
-        id: generateId(),
-      });
+      return [...state, action.payload];
+    },
+    appendNote(state, action) {
+      return [...state, action.payload];
+    },
+    setNotes(state, action) {
+      return action.payload;
     },
     toggleImportanceOf(state, action) {
       const id = action.payload;
@@ -39,5 +27,6 @@ const noteSlice = createSlice({
   },
 });
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export const { createNote, toggleImportanceOf, setNotes, appendNote } =
+  noteSlice.actions;
 export default noteSlice.reducer;
