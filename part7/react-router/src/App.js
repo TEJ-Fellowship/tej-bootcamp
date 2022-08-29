4eb107246110725ae60f87d62835f9e7db034213
rushilshakya <rushil.shakya@gmail.com>
2022-08-29 10:51:12 +0700

    feat: first video codes

--- part7/react-router/src/App.js
+++ part7/react-router/src/App.js
@@ -1,5 +1,101 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import { useState } from "react";
+
+const Home = () => (
+  <div>
+    {" "}
+    <h2>TKTL notes app</h2>{" "}
+  </div>
+);
+
+const Notes = ({ notes }) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+const Users = () => (
+  <div>
+    {" "}
+    <h2>Users</h2>{" "}
+  </div>
+);
+
+const Note = ({ notes }) => {
+  const id = useParams().id;
+  const note = notes.find((n) => n.id === Number(id));
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>{note.important ? "important" : ""}</strong>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
-  return <>Hello</>;
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 3,
+      content: "Most important methods of HTTP-protocol are GET and POST",
+      important: true,
+      user: "Arto Hellas",
+    },
+  ]);
+
+  const [user, setUser] = useState(null);
+
+  const padding = {
+    padding: 5,
+  };
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+      </div>
+      <Routes>
+        <Route path="/notes/:id" element={<Note notes={notes} />} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
