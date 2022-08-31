bb6ce3282be13552dd469f291176d61f6345a153
rushilshakya <rushil.shakya@gmail.com>
2022-08-31 18:05:22 +0700

    feat: complete workshop code

--- part7/custom-hook-counter/src/App.js
+++ part7/custom-hook-counter/src/App.js
@@ -1,26 +1,27 @@
-import useCounter from "./useCounter";
+import { useState } from "react";
+import useField from "./useField";
 
-const App = (props) => {
-  const counter = useCounter();
-  const counterBottom = useCounter();
+const App = () => {
+  const name = useField("text");
+  const born = useField("date");
+  const height = useField("number");
 
   return (
-    <>
+    <div>
+      <form>
+        name:
+        <input {...name} />
+        <br />
+        birthdate:
+        <input {...born} />
+        <br />
+        height:
+        <input {...height} />
+      </form>
       <div>
-        <h1>Top counter</h1>
-        <div>{counter.value}</div>
-        <button onClick={counter.increase}>plus</button>
-        <button onClick={counter.decrease}>minus</button>
-        <button onClick={counter.zero}>zero</button>
+        {name.value} {born.value} {height.value}
       </div>
-      <div>
-        <h1>Bottom counter</h1>
-        <div>{counterBottom.value}</div>
-        <button onClick={counterBottom.increase}>plus</button>
-        <button onClick={counterBottom.decrease}>minus</button>
-        <button onClick={counterBottom.zero}>zero</button>
-      </div>
-    </>
+    </div>
   );
 };
 
