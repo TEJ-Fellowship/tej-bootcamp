d689d6587775e0ca7a15c5ef1e057e603653be44
rushilshakya <rushil.shakya@gmail.com>
2022-09-06 10:50:09 +0545

    feat: complete workshop for part13a

--- part13/notes-server/index.js
+++ part13/notes-server/index.js
@@ -39,8 +39,11 @@ Note.init(
   }
 );
 
+Note.sync();
+
 app.get("/api/notes", async (req, res) => {
   const notes = await Note.findAll();
+  console.log(JSON.stringify(notes, null, 2));
   res.json(notes);
 });
 
@@ -50,6 +53,28 @@ app.post("/api/notes", async (req, res) => {
     return res.json(note);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    console.log(note.toJSON());
+
+    res.json(note);
+  } else {
+    res.status(404).send({ message: "no such note" });
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
   }
 });
 
