c1396892f3713f6a2d97387b26aec96bf9c4a234
rushilshakya <rushil.shakya@gmail.com>
2022-09-09 11:13:11 +0545

    feat: code

--- part13/notes-server/controllers/users.js
+++ part13/notes-server/controllers/users.js
@@ -1,13 +1,23 @@
 const router = require("express").Router();
 
-const { User, Note } = require("../models");
+const { User, Note, Team } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 router.get("/", async (req, res) => {
   const users = await User.findAll({
-    include: {
-      model: Note,
-      // attributes: { exclude: ["userId"] },
-    },
+    include: [
+      {
+        model: Note,
+        // attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
   });
   res.json(users);
 });
@@ -22,8 +32,40 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
+  const user = await User.findByPk(req.params.id, {
+    include: {
+      model: Note,
+    },
+  });
   if (user) {
+    res.json({
+      username: user.username,
+      name: user.name,
+      note_count: user.notes.length,
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  if (!user.admin) {
+    return res.status(401).json({ error: "operation not allowed" });
+  }
+  next();
+};
+
+router.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
+
+  if (user) {
+    user.disabled = req.body.disabled;
+    await user.save();
     res.json(user);
   } else {
     res.status(404).end();
