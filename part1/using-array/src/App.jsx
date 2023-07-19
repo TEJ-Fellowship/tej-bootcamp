170478ae30f217b32a1b4beba205f304ce8bc665
rushilshakya <rushil.shakya@gmail.com>
2023-07-19 11:28:52 +0545

    feat: add ternary statement

--- part1/using-array/src/App.jsx
+++ part1/using-array/src/App.jsx
@@ -2,13 +2,21 @@ import SayHello from "./SayHello";
 
 let App = () => {
   let peopleArray = [
-    { firstName: "Akash", lastName: "Sunar", id: 201 },
-    { firstName: "Sushil", lastName: "Thanet", id: 301 },
-    { firstName: "Hemlal", lastName: "Dulal", id: 401 },
+    // { firstName: "Akash", lastName: "Sunar", id: 201 },
+    // { firstName: "Sushil", lastName: "Thanet", id: 301 },
+    // { firstName: "Hemlal", lastName: "Dulal", id: 401 },
   ];
-  return peopleArray
-    .filter((person) => person.id > 250)
-    .map((value) => <SayHello person={value} key={value.id} />);
+  return (
+    <div>
+      {peopleArray.length > 0 ? (
+        peopleArray
+          .filter((person) => person.id > 250)
+          .map((value) => <SayHello person={value} key={value.id} />)
+      ) : (
+        <h1>No records found</h1>
+      )}
+    </div>
+  );
 };
 
 export default App;
