8201162cb8f4c4f1a1a81336b99f01af704bf045
rushilshakya <rushil.shakya@gmail.com>
2023-07-23 21:32:59 +0545

    feat: delete notes-app

--- part2/notes-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// axios.get("http://localhost:3001/notes1").then(successCallBackFunction).catch(failureCallbackfunction)
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
