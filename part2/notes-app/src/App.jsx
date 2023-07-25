e560817535601ce98005042a03fd02d5d8d0f678
rushilshakya <rushil.shakya@gmail.com>
2023-07-25 10:48:39 +0545

    feat: part2-b first part code

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setNotes(
+      notes.concat({
+        content: newNote,
+        id: notes.length + 1,
+        important: Math.random() > 0.5,
+      })
+    );
+    setNewNote("");
+    console.log("form has been submitted");
+  };
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
     <>
       <h1>Notes</h1>
@@ -9,6 +31,10 @@ const App = ({ notes }) => {
           return <Note key={value.id} note={value} />;
         })}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <input value={newNote} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
     </>
   );
 };
