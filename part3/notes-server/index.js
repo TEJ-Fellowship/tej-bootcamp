b27022ad22061ad44c7be9a987f6dcb50184242b
rushilshakya <rushil.shakya@gmail.com>
2023-08-02 11:03:20 +0545

    feat: update code for deployment

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -30,10 +33,6 @@ let notes = [
     important: true,
   },
 ];
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello world</h1>");
-});
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
@@ -87,6 +86,6 @@ app.use((request, response, next) => {
   response.status(404).send("no code available to hande this request");
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT ? process.env.PORT : 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
