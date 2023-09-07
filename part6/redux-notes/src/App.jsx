3cb2c279d2d70f9010414d8f8fbec7d8998701cf
rushilshakya <rushil.shakya@gmail.com>
2023-09-07 11:08:48 +0545

    feat: refactor filter to own component

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,45 +1,15 @@
 // import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
 
 import NoteForm from "./components/NoteForm";
 import Notes from "./components/Notes";
-import { filterChange } from "./reducers/filterReducer";
+import VisibilityFilter from "./components/VisibilityFilter";
 
 const App = () => {
   // const [filter, setFilter] = useState("ALL");
-  const dispatch = useDispatch();
-  const filter = useSelector((state) => {
-    return state.filter;
-  });
 
-  const filterSelected = (filter) => {
-    console.log("clicked radio is", filter);
-    // setFilter(filter);
-    dispatch(filterChange(filter));
-  };
   return (
     <div>
-      <div>
-        all
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected("ALL")}
-          checked={filter === "ALL"}
-        />
-        important
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected("IMPORTANT")}
-        />
-        nonimportant
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected("NONIMPORTANT")}
-        />
-      </div>
+      <VisibilityFilter />
       <NoteForm />
       <Notes />
     </div>
