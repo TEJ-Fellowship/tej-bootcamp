9b2759a292b913bbb110f6baae29a483d1f0e4c7
rushilshakya <rushil.shakya@gmail.com>
2023-09-13 11:36:39 +0545

    feat: part6-d video2

--- part6/redux-notes/src/index.jsx
+++ part6/redux-notes/src/index.jsx
@@ -1,13 +1,16 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
+import App from "./ReactQueryApp";
+// import { Provider } from "react-redux";
+// import store from "./store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+const queryClient = new QueryClient();
+
 root.render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>
+  </QueryClientProvider>
 );
