b4a79841e8f01fb61de6177009bec4fdacb64055
rushilshakya <rushil.shakya@gmail.com>
2023-09-20 11:03:42 +0545

    feat: code for part-7-c video-2

--- part7/react-router/src/App.jsx
+++ part7/react-router/src/App.jsx
@@ -3,7 +3,15 @@ import Notes from "./Notes";
 import Note from "./Note";
 import { useState } from "react";
 import Login from "./Login";
-import { Alert, Navbar, Nav } from "react-bootstrap";
+// import { Navbar, Nav } from "react-bootstrap";
+import {
+  Container,
+  Alert,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+} from "@mui/material";
 
 const notes = [
   {
@@ -48,38 +56,27 @@ const App = () => {
   const footerStyle = { color: "blue", fontSize: "20px" };
 
   return (
-    <div className="container">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/">
-                home
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/notes">
-                notes
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/users">
-                users
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              {user ? (
-                <em style={padding}>{user} logged in</em>
-              ) : (
-                <Link style={padding} to="/login">
-                  login
-                </Link>
-              )}
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
+            home
+          </Button>
+          <Button color="inherit" component={Link} to="/notes">
+            notes
+          </Button>
+          <Button color="inherit" component={Link} to="/users">
+            users
+          </Button>
+          {user ? (
+            <em>{user} logged in</em>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
 
       <Routes>
         <Route path="/notes/:id" element={<Note note={note} />} />
@@ -95,7 +92,7 @@ const App = () => {
       <div>
         <i style={footerStyle}>Note app, Department of Computer Science 2023</i>
       </div>
-    </div>
+    </Container>
   );
 };
 
