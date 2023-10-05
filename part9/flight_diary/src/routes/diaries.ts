e0f087ca5ae7026006dbf343ae73eeb72af34372
rushilshakya <rushil.shakya@gmail.com>
2023-10-05 12:07:36 +0545

    feat: fix a few problems

--- part9/flight_diary/src/routes/diaries.ts
+++ part9/flight_diary/src/routes/diaries.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 import diaryService from "../services/diaryService";
 import toNewDiaryEntry from "../utils";
@@ -20,9 +19,18 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const newDiaryEntry = toNewDiaryEntry(req.body);
-  const addedEntry = diaryService.addDiary(newDiaryEntry);
-  res.json(addedEntry);
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
