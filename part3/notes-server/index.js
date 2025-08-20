7686cadca4c286d1ceae9be16e68190a2baabe1b
Sanjeev Rai <sunzeevraee@gmail.com>
2025-08-20 18:41:35 +0545

    Part 3c, Half Completed

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-// app.use(unknownEndpoint);
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -20,23 +16,39 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+//-------------------------------Copy start
+
+const url = `mongodb+srv://sanjeev418rai:password12345@cluster0.9ihbd46.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(url);
+
+//--------------------------------------
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// const note = new Note({
+//   content: "JS is not so easy.",
+//   important: false,
+// });
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+// Note.findById("68a5a4eedbd1465af20f4274").then((result) => {
+//   console.log(result);
+//   mongoose.connection.close();
+// });
+
+//------------------------------------
+
+let notes = [];
 
 // const app = http.createServer((request, response) => {
 //   response.writeHead(200, { "Content-Type": "application/json" });
@@ -44,8 +56,9 @@ let notes = [
 // });
 
 app.get("/api/notes", (request, response) => {
-  console.log("you are calling get for all notes");
-  response.json(notes);
+  Note.find({}).then((result) => {
+    response.status(200).send(result);
+  });
 });
 
 app.get("/api/notes/:noteid", (request, response) => {
@@ -83,6 +96,12 @@ app.post("/api/notes", (request, response) => {
   notes.push(myNote);
   response.status(201).json(myNote);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT ? process.env.PORT : 3001;
 app.listen(PORT);
