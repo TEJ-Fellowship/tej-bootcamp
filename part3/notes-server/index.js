d5b70408b9d391d95b66e6cc1f222d193870cea1
Sanjeev Rai <sunzeevraee@gmail.com>
2025-08-22 10:17:42 +0545

    Part 3-c completed

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
+const Note = require("./model/note");
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -16,85 +17,58 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-//-------------------------------Copy start
-
-const url = `mongodb+srv://sanjeev418rai:password12345@cluster0.9ihbd46.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.connect(url);
-
-//--------------------------------------
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-const Note = mongoose.model("Note", noteSchema);
-
-// const note = new Note({
-//   content: "JS is not so easy.",
-//   important: false,
-// });
-
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
-
-// Note.findById("68a5a4eedbd1465af20f4274").then((result) => {
-//   console.log(result);
-//   mongoose.connection.close();
-// });
-
-//------------------------------------
-
 let notes = [];
 
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { "Content-Type": "application/json" });
-//   response.end(JSON.stringify(notes));
-// });
-
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((result) => {
-    response.status(200).send(result);
-  });
-});
-
-app.get("/api/notes/:noteid", (request, response) => {
-  const myId = request.params.noteid;
-  const myNote = notes.find((note) => note.id === myId);
-
-  if (myNote) {
-    response.json(myNote);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.delete("/api/notes/:noteid", (request, response) => {
-  const myId = request.params.noteid;
-  notes = notes.filter((note) => note.id !== myId);
-
-  response.status(204).end();
+app.get("/api/notes", (request, response, next) => {
+  Note.find({})
+    .then((result) => {
+      response.status(200).send(result);
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  note.id = String(notes.length + 1);
-  if (!note.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
   }
-  console.log("our note is", note);
-  const myNote = {
-    content: note.content,
-    important: note.important || false,
-    id: String(notes.length + 1),
-  };
-  notes.push(myNote);
-  response.status(201).json(myNote);
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+app.get("/api/notes/:noteid", (request, response, next) => {
+  const id = request.params.noteid;
+
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log("error happening", error);
+      next(error);
+    });
+});
+
+app.delete("/api/notes/:noteid", (request, response) => {
+  const id = request.params.noteid;
+
+  const deletedNote = Note.findByIdAndDelete(id);
+
+  deletedNote.then(() => {
+    response.status(200).send(`The note with id ${id} successfully deleted`);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -102,6 +76,19 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const PORT = process.env.PORT ? process.env.PORT : 3001;
 app.listen(PORT);
