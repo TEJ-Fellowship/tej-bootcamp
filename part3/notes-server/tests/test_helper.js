25c981d65bac1c082d39b7ff8dfef83e80205331
Sanjeev Rai <sunzeevraee@gmail.com>
2025-08-27 19:42:57 +0545

    feat: Part 4-c completed

--- part3/notes-server/tests/test_helper.js
+++ part3/notes-server/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Note = require("../model/note");
+const User = require("../model/user");
 
 const initialNotes = [
   {
@@ -24,8 +25,14 @@ const notesInDb = async () => {
   return notes.map((note) => note.toJSON());
 };
 
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((user) => user.toJSON());
+};
+
 module.exports = {
   initialNotes,
   nonExistingId,
   notesInDb,
+  usersInDb,
 };
