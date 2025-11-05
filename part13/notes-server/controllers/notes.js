9cb4fe2e4486a3ca3177f087dce27ff6a75b475a
rushilshakya <rushil.shakya@gmail.com>
2025-11-05 11:13:21 +0545

    feat: part13-b video 3 code

--- part13/notes-server/controllers/notes.js
+++ part13/notes-server/controllers/notes.js
@@ -1,15 +1,37 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
 
 const { Note } = require("../models");
+const { User } = require("../models");
 
 router.get("/", async (req, res) => {
   const notes = await Note.findAll();
   res.json(notes);
 });
 
-router.post("/", async (req, res) => {
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
+
+router.post("/", tokenExtractor, async (req, res) => {
   try {
-    const note = await Note.create(req.body);
+    const user = await User.findByPk(req.decodedToken.id);
+    const note = await Note.create({
+      ...req.body,
+      userId: user.id,
+      date: new Date(),
+    });
     res.json(note);
   } catch (error) {
     return res.status(400).json({ error });
